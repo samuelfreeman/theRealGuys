7be6b6314e2902bb847fe8249e2dde2d206fbb5b
samuelfreeman <samuelmanueljnr@gmail.com>
2024-06-07 01:25:41 +0000

    first commit

---
+++ assets/NAVBAR.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NAVBAR = () => {
+  return (
+   <div>
+    <nav>
+      
+      <h1>
+        The Real Guys 
+      </h1>
+    </nav>
+   </div>
+  );
+};
+
+export default NAVBAR;
